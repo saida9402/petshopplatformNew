1a3e34804776ef7d381ae0a3b5f60ece6101a878
saida9402 <saidahaymatova@gmail.com>
2026-04-29 06:56:12 +0900

    feat: added order.input dto

--- apps/petoria-api/src/libs/dto/order/order.input.ts
+++ apps/petoria-api/src/libs/dto/order/order.input.ts
@@ -1,31 +1,43 @@
-// src/components/order/dto/order.input.ts
-
 import { Field, ID, InputType, Int } from '@nestjs/graphql';
+import { IsInt, IsNotEmpty, IsOptional, Length, Min } from 'class-validator';
 import { OrderPaymentMethod } from '../../../libs/enums/order.enum';
 
 @InputType()
 export class OrderItemInput {
+	@IsNotEmpty()
 	@Field(() => ID)
 	productId: string;
 
+	@IsNotEmpty()
+	@IsInt()
+	@Min(1)
 	@Field(() => Int)
 	itemQuantity: number;
 
+	@IsNotEmpty()
+	@IsInt()
+	@Min(0)
 	@Field(() => Int)
 	itemPrice: number;
 }
 
 @InputType()
 export class OrderInput {
+	@IsNotEmpty()
 	@Field(() => [OrderItemInput])
 	orderItems: OrderItemInput[];
 
+	@IsNotEmpty()
 	@Field(() => OrderPaymentMethod)
 	paymentMethod: OrderPaymentMethod;
 
-	@Field()
+	@IsNotEmpty()
+	@Length(5, 200)
+	@Field(() => String)
 	orderAddress: string;
 
+	@IsOptional()
+	@Length(0, 300)
 	@Field({ nullable: true })
 	orderNote?: string;
 }
